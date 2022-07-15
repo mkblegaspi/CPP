e774137f0ab6b73e69293f42e7196a4154f58369
mkblegaspi <mklegaspi@hotmail.com>
2022-07-15 19:19:13 +0800

    Member Functions and Interface

---
+++ Learning/C++ in Action/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+class InputNum
+{
+	public:
+		InputNum()
+		{
+			std::cout << "Enter number ";
+			std::cin >> _num;
+		}
+
+	int GetValue() const
+	{
+		return _num;
+	}
+	private:
+		int _num;
+};
+
+int main()
+{
+	InputNum num;
+	std::cout << "The value is " << num.GetValue() << std::endl;
+	return 0;
+}
